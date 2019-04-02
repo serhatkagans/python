192a18ae58d42cf4eb9388d33d7069afec3ba2df
serhatkagans <47238858+serhatkagans@users.noreply.github.com>
2019-04-02 21:56:45 +0300

    Add files via upload

---
+++ 2- Koşullu İfadeler/21 saat.py
@@ -0,0 +1,10 @@
+saat=int(input("lüften saati giriniz"))
+
+if saat<7 or saat>7: 
+   print("Alarm çalmıyor.") 
+
+elif saat=7: 
+   print("Alarm çalıyor.") 
+
+else: 
+   print("Lütfen bir saat giriniz.")
